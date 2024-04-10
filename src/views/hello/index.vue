b308af254ee41d49a9a1aa1aa2cf97fd58138ad1
阮阳栋 <14192765+RyotoTannhauser@user.noreply.gitee.com>
2024-04-10 14:53:34 +0800

    load vue-element-admin

---
+++ src/views/hello/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <iframe
+      src="https://www.migufun.com/miguplay/middleGame/gameplay/feature/650786758"
+      width="100%"
+      height="500px"
+      frameborder="0"
+      allowfullscreen
+    />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'WebpageFrame'
+}
+</script>
+
+<style>
+/* 你的样式 */
+</style>
