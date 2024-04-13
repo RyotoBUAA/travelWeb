cc2ae104ff2520c4d1f1105e42cb231fde6f8ef2
云惟彬 <21377079>
2024-04-13 19:58:22 +0800

    map

--- src/views/map/index.vue
+++ src/views/map/index.vue
@@ -1,5 +1,7 @@
 <template>
   <div>
+    <el-button :loading="loading" type="primary" @click.native.prevent="generate">推荐地点</el-button>
+    <el-button :loading="loading" type="primary" @click.native.prevent="reset">重置</el-button>
     <div id="container" />
   </div>
 </template>
@@ -8,6 +10,13 @@
 import AMapLoader from '@amap/amap-jsapi-loader'
 export default {
   name: 'Map',
+  data() {
+    return {
+      selected: [],
+      pos: [],
+      recommend: []
+    }
+  },
   mounted() {
     this.initAMap()
   },
@@ -16,10 +25,13 @@ export default {
   },
   methods: {
     initAMap() {
+      window._AMapSecurityConfig = {
+        securityJsCode: '0d37e1f29749bb678758012e06c919d1'
+      }
       AMapLoader.load({
         key: 'ae61507fe14e303c0b6e8fb0ecaea2ff', // 申请好的Web端开发者Key，首次调用 load 时必填
         version: '2.0', // 指定要加载的 JSAPI 的版本，缺省时默认为 1.4.15
-        plugins: ['AMap.Scale'] // 需要使用的的插件列表，如比例尺'AMap.Scale'，支持添加多个如：['...','...']
+        plugins: ['AMap.Scale', 'AMap.PlaceSearch', 'AMap.AutoComplete', 'AMap.ToolBar', 'AMap.MouseTool', 'AMap.Marker'] // 需要使用的的插件列表，如比例尺'AMap.Scale'，支持添加多个如：['...','...']
       })
         .then((AMap) => {
           this.map = new AMap.Map('container', {
@@ -28,14 +40,53 @@ export default {
             zoom: 11, // 初始化地图级别
             center: [116.397428, 39.90923] // 初始化地图中心点位置
           })
+          this.map.on('click', (e) => {
+            const marker = new AMap.Marker({
+              position: new AMap.LngLat(e.lnglat.lng, e.lnglat.lat), // 经纬度对象，也可以是经纬度构成的一维数组[116.39, 39.9]
+              title: ''
+            })
+            marker.setMap(this.map)
+            marker.setIcon('//a.amap.com/jsapi_demos/static/demo-center/icons/poi-marker-red.png')
+            this.selected.push(marker)
+            this.pos.push([e.lnglat.lng, e.lnglat.lat])
+          })
         })
         .catch((e) => {
           console.log(e)
         })
+    },
+    generate() {
+      var latsum = 0
+      var lngsum = 0
+      this.pos.forEach(p => {
+        lngsum += p[0]
+        latsum += p[1]
+      })
+      if (this.recommend.length == 0) {
+        const marker = new AMap.Marker({
+          position: new AMap.LngLat(lngsum / this.pos.length + Math.random() * 0.03, latsum / this.pos.length + Math.random() * 0.03), // 经纬度对象，也可以是经纬度构成的一维数组[116.39, 39.9]
+          title: '推荐地点'
+        })
+        marker.setMap(this.map)
+        marker.setIcon('//a.amap.com/jsapi_demos/static/demo-center/icons/poi-marker-red.png')
+        this.recommend.push(marker)
+      }
+    },
+    reset() {
+      this.selected.forEach(marker => {
+        marker.setMap(null)
+        marker = null
+      })
+      this.recommend.forEach(marker => {
+        marker.setMap(null)
+        marker = null
+      })
+      this.selected = []
+      this.pos = []
+      this.recommend = []
     }
   }
 }
-
 </script>
 
 <style scoped>
